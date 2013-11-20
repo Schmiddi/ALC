6ce9dd25c2d02bbdf2d13174462954f8f0ef4577
Dennis Schmidt <schmiddi11@gmx.de>
2013-11-20 18:23:41 +0100

    Add loops to iterate through parameter settings
    Additionally configure the axis values for the plot

--- src/GeneratesPlot.java
+++ src/GeneratesPlot.java
@@ -1,7 +1,6 @@
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.awt.image.TileObserver;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -23,19 +22,6 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 
 public class GeneratesPlot {
-
-	final String title;
-	
-	/**
-	 * Creates a new demo.
-	 * 
-	 * @param title
-	 *            the frame title.
-	 */
-	public GeneratesPlot(final String title) {
-		this.title = title;
-	}
-	
 
 	/**
 	 * Creates a sample dataset.
@@ -65,13 +51,13 @@ public class GeneratesPlot {
 	 * 
 	 * @return a chart.
 	 */
-	private JFreeChart createChart(final XYDataset dataset) {
+	private JFreeChart createChart(final XYDataset dataset, String title) {
 
 		// create the chart...
 		final JFreeChart chart = ChartFactory.createXYLineChart(
 				title, // chart title
 				"Threshold", // x axis label
-				"Y", // y axis label
+				"UDA", // y axis label
 				dataset, // data
 				PlotOrientation.VERTICAL, 
 				false, // no legend
@@ -90,27 +76,27 @@ public class GeneratesPlot {
 		// change the auto tick unit selection to integer units only...
 		// X-Axis
 		NumberAxis domain = (NumberAxis) plot.getDomainAxis();
-		domain.setRange(0.00, 13.00);
-		domain.setTickUnit(new NumberTickUnit(1));
+		domain.setRange(0.00, 0.01);
+		domain.setTickUnit(new NumberTickUnit(0.001));
 		domain.setVerticalTickLabels(false);
 
 		// Y-Axis
 		NumberAxis range = (NumberAxis) plot.getRangeAxis();
-		range.setRange(0.0, 11.0);
-		range.setTickUnit(new NumberTickUnit(1));
+		range.setRange(0.4, 1.0);
+		range.setTickUnit(new NumberTickUnit(0.1));
 		range.setVerticalTickLabels(false); // Horizontal Alignment
 
 		return chart;
 	}
 
-	public void create(List <List> myList) {
+	public void create(List <List> myList, String filename) {
 
 		// Get the Data
 		final XYDataset dataset = createDataset(myList);
-		final JFreeChart chart = createChart(dataset);
+		final JFreeChart chart = createChart(dataset,filename);
 
 		// Size: Width, Height
-		BufferedImage objBufferedImage = chart.createBufferedImage(600, 300);
+		BufferedImage objBufferedImage = chart.createBufferedImage(1000, 600);
 
 		ByteArrayOutputStream bas = new ByteArrayOutputStream();
 		try {
@@ -118,7 +104,7 @@ public class GeneratesPlot {
 			byte[] byteArray = bas.toByteArray();
 			InputStream in = new ByteArrayInputStream(byteArray);
 			BufferedImage image = ImageIO.read(in);
-			File outputfile = new File("image.png"); // File Name
+			File outputfile = new File(filename+".png"); // File Name
 			ImageIO.write(image, "png", outputfile);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -132,21 +118,21 @@ public class GeneratesPlot {
 	 *            ignored.
 	 */
 	public static void main(final String[] args) {
-		final GeneratesPlot demo = new GeneratesPlot("Line Chart Demo 6");
+		final GeneratesPlot demo = new GeneratesPlot();
 		
 		List <List>myList = new ArrayList<List>();
 		List <Object>data = new ArrayList<Object>();
-		data.add(11.1);
-		data.add(2.2);
+		data.add(0.0003);
+		data.add(0.3);
 		data.add(5461);
 		myList.add(data);
 		data = new ArrayList<Object>();
-		data.add(0.5);
+		data.add(0.008);
 		data.add(0.5);
 		data.add(4571);
 		myList.add(data);
 		
-		demo.create(myList);
+		demo.create(myList,"Test");
 	}
 
 }
---
+++ src/MineMulti.java
@@ -0,0 +1,182 @@
+import weka.core.Instances;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Random;
+
+public class MineMulti {
+	// final static String FolderWithDropbox = "C:\\Users\\IBM_ADMIN";
+	final static String FolderWithDropbox = "E:\\Downloads";
+
+	enum options {
+		Ngram, LowerCase, NormalizeDocLength, Stemming, OutputWordCounts, IDFTransform, TFTransform, Stopword, BinarizeNumericAttributes
+	}
+
+	public static void main(String[] args) throws Exception {
+		/**
+		 * 
+		 * Set parameter of input function
+		 */
+		Map<options, Boolean> settings = new HashMap<options, Boolean>();
+		List<Double> threshold = new ArrayList<Double>();
+
+		// parameters for TextDirectoryLoader
+		String currDir = FolderWithDropbox
+				+ "\\Dropbox\\Detecting Alcohol Intoxication in Speech\\Felix\\Backup\\DP_real\\rawData\\DP";
+		String title;
+
+		// Set Thresholds
+		threshold.add(0.0001);
+		threshold.add(0.0003);
+		threshold.add(0.0006);
+		threshold.add(0.001);
+		threshold.add(0.003);
+		threshold.add(0.004);
+		threshold.add(0.005);
+		threshold.add(0.006);
+		threshold.add(0.007);
+		threshold.add(0.008);
+		threshold.add(0.01);
+
+		List<Boolean> values = new ArrayList<Boolean>();
+		values.add(true);
+		values.add(false);
+		List<Integer> ngram = new ArrayList<Integer>();
+		ngram.add(2);
+		ngram.add(3);
+		List<Integer> stop = new ArrayList<Integer>();
+		stop.add(1);
+		stop.add(2);
+		stop.add(3);
+		// parameters for StringToVector
+
+		int WordsToKeep = 1000000;
+		settings.put(options.Ngram, true);
+		// Boolean Ngram = true; // True/False
+		int ngram_min = 1;
+		String list1 = null;
+		for (int NG : ngram) {
+			for (Boolean LC : values) {
+				for (Boolean ST : values) {
+					for (Boolean NDL : values) {
+						for (Boolean BNA : values) {
+							for (int SW : stop) {
+								int ngram_max = NG;// 2/3
+
+								settings.put(options.LowerCase, LC);
+								settings.put(options.Stemming, ST);
+
+								settings.put(options.NormalizeDocLength, NDL);
+								settings.put(options.OutputWordCounts, NDL);
+								settings.put(options.IDFTransform, NDL);
+								settings.put(options.TFTransform, NDL);
+								settings.put(options.BinarizeNumericAttributes,
+										BNA);
+
+								if (SW == 1) {
+									settings.put(options.Stopword, true);
+									list1 = "resources\\germanST.txt";
+								} else if (SW == 2) {
+									settings.put(options.Stopword, true);
+									list1 = "resources\\stop.txt";
+								} else {
+									settings.put(options.Stopword, false);
+								}
+								// parameters for Attribute Selection
+
+								// stop.txt / germanST.txt
+
+								MineMulti.runMine(currDir, WordsToKeep,
+										ngram_min, ngram_max, list1, threshold,
+										settings);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	public static void runMine(String currDir, int WordsToKeep, int ngram_min,
+			int ngram_max, String list1, List<Double> threshold,
+			Map<options, Boolean> settings) throws Exception {
+
+		/*
+		 * 
+		 * All parameter are set
+		 */
+
+		String[] fullPath = currDir.split("/");
+		String fileName = fullPath[fullPath.length - 1];
+
+		MyOutput text_data;
+		Instances dataRaw;
+		MyOutput filtered;
+		Instances dataFiltered;
+		MyOutput selected;
+		Instances dataSelected;
+		MyClassificationOutput logistic;
+		int t;
+
+		// Load data to Weka
+		text_data = WekaMagic.loadText(currDir);
+		text_data.print();
+		dataRaw = text_data.getData();
+
+		// Store raw data from Weka to arff file
+		WekaMagic.saveToArff(dataRaw, fileName + "_raw_text", text_data);
+
+		// Generate the features
+		filtered = WekaMagic.generateFeatures(dataRaw, WordsToKeep,
+				settings.get(options.Ngram), ngram_min, ngram_max,
+				settings.get(options.LowerCase),
+				settings.get(options.NormalizeDocLength),
+				settings.get(options.Stemming),
+				settings.get(options.OutputWordCounts),
+				settings.get(options.IDFTransform),
+				settings.get(options.TFTransform),
+				settings.get(options.Stopword), list1);
+		filtered.print();
+		dataFiltered = filtered.getData();
+		// Randomize data, because they are initially sorted
+		dataFiltered.randomize(new Random(1));
+
+		// Store featured data from Weka to arff file
+		WekaMagic.saveToArff(dataFiltered, fileName + "_featured", filtered);
+
+		Map<Double, ArrayList<Object>> results = new HashMap<Double, ArrayList<Object>>();
+
+		for (double currentThreshold : threshold) {
+
+			// Run selection
+			selected = WekaMagic.selectionByInfo(dataFiltered,
+					settings.get(options.BinarizeNumericAttributes),
+					currentThreshold);
+			selected.print();
+			dataSelected = selected.getData();
+
+			// Backup the selection
+			WekaMagic.saveToArff(dataSelected, fileName + "_selected_t"
+					+ currentThreshold, selected);
+
+			// Run ML algorithm - logistic
+			logistic = WekaMagic.runLogistic(dataSelected);
+			logistic.print();
+			WekaMagic.saveToArff(null, fileName + "_logistic_t"
+					+ currentThreshold, logistic);
+
+			// Result processing
+			ArrayList<Object> al = new ArrayList<Object>();
+			al.add(0, logistic.getUAR());
+			al.add(1, logistic.getElapsedTime());
+			results.put(currentThreshold, al);
+		}
+		// Store treshold, UAR
+		ArrayList<Integer> tl = new ArrayList<Integer>();
+		tl.add(0, 1);
+		tl.add(1, 2);
+		WekaMagic.printHashMap(results, tl, fileName + "_results_2.csv");
+	}
+}
