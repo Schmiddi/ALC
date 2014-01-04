7df73754271a1678766991f85ae9da4dc0ceb77a
FelixNeutatz <neutatz@googlemail.com>
2014-01-04 01:05:57 +0100

    Plotting functions for learning curve (has to be improved!!)

--- src/GeneratesPlot.java
+++ src/GeneratesPlot.java
@@ -109,6 +109,83 @@ public class GeneratesPlot {
 			e.printStackTrace();
 		}
 	}
+	
+	
+	private static XYDataset createDatasetLC(List <List<Double>> myList) {
+		final XYSeries series_train = new XYSeries("Train");
+		final XYSeries series_cross_validation = new XYSeries("Cross-Validation");
+		for(List<Double> tempList : myList){
+								//			percentage of test instances, 		FMeasure
+			series_train.add(					tempList.get(6),				tempList.get(2));
+			series_cross_validation.add(		tempList.get(6), 				tempList.get(4));
+		}
+
+		final XYSeriesCollection dataset = new XYSeriesCollection();
+		dataset.addSeries(series_train);
+		dataset.addSeries(series_cross_validation);
+		return dataset;
+
+	}
+	
+	private static JFreeChart createChartLC(final XYDataset dataset, String title) {
+
+		// create the chart...
+		final JFreeChart chart = ChartFactory.createXYLineChart(
+				title, // chart title
+				"m (in %)- training set size", // x axis label
+				"FScore", // y axis label
+				dataset, // data
+				PlotOrientation.VERTICAL, 
+				false, // no legend
+				true, // tooltips
+				false // urls
+				);
+
+		chart.setBackgroundPaint(Color.white);
+
+		// get a reference to the plot for further customisation...
+		final XYPlot plot = chart.getXYPlot();
+		plot.setBackgroundPaint(Color.lightGray);
+		plot.setDomainGridlinePaint(Color.white);
+		plot.setRangeGridlinePaint(Color.white);
+
+		// change the auto tick unit selection to integer units only...
+		// X-Axis
+		NumberAxis domain = (NumberAxis) plot.getDomainAxis();
+		domain.setRange(0.00, 0.01);
+		domain.setTickUnit(new NumberTickUnit(0.001));
+		domain.setVerticalTickLabels(false);
+
+		// Y-Axis
+		NumberAxis range = (NumberAxis) plot.getRangeAxis();
+		range.setRange(0.4, 1.0);
+		range.setTickUnit(new NumberTickUnit(0.1));
+		range.setVerticalTickLabels(false); // Horizontal Alignment
+
+		return chart;
+	}
+	
+	public static void printLearningCurve(List <List<Double>> myList, String outputFolder, String filename) {
+
+		// Get the Data
+		final XYDataset dataset = createDatasetLC(myList);
+		final JFreeChart chart = createChart(dataset,filename);
+
+		// Size: Width, Height
+		BufferedImage objBufferedImage = chart.createBufferedImage(1000, 600);
+
+		ByteArrayOutputStream bas = new ByteArrayOutputStream();
+		try {
+			ImageIO.write(objBufferedImage, "png", bas); // File Type
+			byte[] byteArray = bas.toByteArray();
+			InputStream in = new ByteArrayInputStream(byteArray);
+			BufferedImage image = ImageIO.read(in);
+			File outputfile = new File(outputFolder + filename+".png"); // File Name
+			ImageIO.write(image, "png", outputfile);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Starting point for the demonstration application.
