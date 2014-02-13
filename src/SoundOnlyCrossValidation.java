f248556a207189bce807987939646ce08f4532e8
FelixNeutatz <neutatz@googlemail.com>
2014-02-13 18:03:03 +0100

    new directory structure

--- src/SoundOnlyCrossValidation.java
+++ src/SoundOnlyCrossValidation.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 
 import weka.classifiers.functions.Logistic;
 import weka.core.Debug;
@@ -23,35 +24,33 @@ public class SoundOnlyCrossValidation {
 		boolean isWindows = ((System.getProperty("os.name").contains("Windows")))?true:false;
 		String fileSep = isWindows?"\\":"/";
 		
-		
 		try {
 			Instances data = null;
-			//data = testRun.getSoundInstances(ARFF_FILE); //get all instances from arff file
 			
-			String csv_dir = args[0];
+			String arff_dir = args[0];
+			String csv_dir = getParent(arff_dir);
 			
-			data = SoundOnly.getSoundInstances(csv_dir + fileSep + "sound", fileSep + "output.csv");
+			data = SoundOnly.getSoundInstances(arff_dir, csv_dir + "output.csv");
 			
-			System.out.println("Instances read from " + csv_dir + ": " + data.numInstances());
-			List<List<List<Double>>> results = testRun.runTest(data);
+			System.out.println("Instances read from " + arff_dir + ": " + data.numInstances());
+			List<List<List<Double>>> results = testRun.runTestUAR(data);
 			
 			
-			//Adjust file path
-			csv_dir += fileSep;
+			arff_dir += fileSep;
 			
 			//save to CSV
-			WekaMagic.printHashMap(results.get(0), csv_dir + "result_train_cross_validation.csv");//Train set
-			WekaMagic.printHashMap(results.get(1), csv_dir + "result_cross_cross_validation.csv");//Cross set
+			WekaMagic.printHashMap(results.get(0), arff_dir + "result_train_cross_validation.csv");//Train set
+			WekaMagic.printHashMap(results.get(1), arff_dir + "result_cross_cross_validation.csv");//Cross set
 			
 					
 			//Plot everything
 			System.out.println("Plotting results...");
 			
-			System.out.println("Creating chart " + csv_dir + "plot_train_cross_validation.png ...");
-			GeneratesPlot.createSound(results.get(0), csv_dir, "plot_train_cross_validation.png");
+			System.out.println("Creating chart " + arff_dir + "plot_train_cross_validation.png ...");
+			GeneratesPlot.createSound(results.get(0), arff_dir, "plot_train_cross_validation.png");
 			
-			System.out.println("Creating chart " + csv_dir + "plot_test_cross_validation.png ...");
-			GeneratesPlot.createSound(results.get(1), csv_dir, "plot_test_cross_validation.png");
+			System.out.println("Creating chart " + arff_dir + "plot_test_cross_validation.png ...");
+			GeneratesPlot.createSound(results.get(1), arff_dir, "plot_test_cross_validation.png");
 			
 			System.out.println("Finished operations");
 			
@@ -86,8 +85,8 @@ public class SoundOnlyCrossValidation {
 			CrossValidationOutput cvo = WekaMagic.crossValidation(currentResult, data, 10, 1, null);
 			
 			
-			String modelDir = "neu";
-			Debug.saveToFile(modelDir, cvo.getBestClassifier());
+			//String modelDir = "neu";
+			//Debug.saveToFile(modelDir, cvo.getBestClassifier());
 			
 			// Result processing to lists
 			List<Double> exTrain = new ArrayList<Double>();
@@ -108,5 +107,78 @@ public class SoundOnlyCrossValidation {
 		
 		return values;
 				
+	}
+	
+private List<List<List<Double>>> runTestUAR(Instances data) throws Exception {
+		
+		/* Runs 10 times logistic to regularize and stores results in list */
+		
+		List<List<List<Double>>> values = new ArrayList<List<List<Double>>>();
+		
+		double stdRidge = 0.00000001; //10^-8
+		double currentRidge = stdRidge;
+		MyClassificationOutput currentResult = null;
+		
+		//create 3 lists storing data for 3 sets
+		List<List<Double>> listTrain = new ArrayList<List<Double>>();
+		List<List<Double>> listCross = new ArrayList<List<Double>>();
+		
+		System.out.println("Running tests for train, cross and test set...");
+		//Iterate through different ridge values
+		for (int i=0;i<15;i++)
+		{
+			currentRidge = stdRidge * (Math.pow(10, i));
+			
+			System.out.println("Cross validation for ridge = " + currentRidge);
+			currentResult = WekaMagic.runLogistic(null, currentRidge, 5);
+			CrossValidationOutput cvo = WekaMagic.crossValidation(currentResult, data, 10, 1, null);
+			
+			
+			// Result processing to lists
+			List<Double> exTrain = new ArrayList<Double>();
+			List<Double> exCross = new ArrayList<Double>();
+			
+			exTrain.add(0, currentRidge);
+			exTrain.add(1, cvo.getTrainUAR());
+			listTrain.add(exTrain);
+			
+			exCross.add(0, currentRidge);
+			exCross.add(1, cvo.getTestUAR());
+			listCross.add(exCross);
+			
+		}
+		
+		values.add(listTrain);
+		values.add(listCross);
+		
+		return values;
+				
+	}
+	
+	public static String getParent(String dir){
+		boolean isWindows = ((System.getProperty("os.name").contains("Windows")))?true:false;
+		String fileSep = isWindows?"\\":"/";
+		
+		String parent;
+		
+		StringTokenizer Tok = new StringTokenizer(dir,fileSep);
+		int n=0;
+		while (Tok.hasMoreElements()) {
+			Tok.nextElement();
+			n++;
+		}
+		
+		Tok = new StringTokenizer(dir,fileSep);
+		int i=0;
+		parent = "";
+	    while (Tok.hasMoreElements()){
+	    	if(i<n-1){
+	                parent += Tok.nextElement() + fileSep+ fileSep;
+	    	}
+	    	else{break;}
+	    	i++;
+	    }
+	    
+	    return parent;
 	}
 }
