f195b0cb595b2fb5dffb6fe20b10aac938bf7d6e
FelixNeutatz <neutatz@googlemail.com>
2014-06-26 13:45:15 +0200

    Experiment with single tasks only

---
+++ src/team2014/test/Experiments.java
@@ -0,0 +1,155 @@
+package team2014.test;
+import java.util.ArrayList;
+
+import team2014.weka.CrossValidationOutput;
+import team2014.weka.WekaMagic;
+import team2014.weka.speaker.Speaker;
+import team2014.weka.speaker.SpeakerCrossValidation;
+import team2014.weka.speaker.SpeakerSamples;
+import team2014.weka.speaker.SpeakerSet;
+import weka.core.Instances;
+import weka.filters.Filter;
+
+
+public class Experiments {
+	public static void main(String[] args) {
+		
+		
+			boolean isWindows = ((System.getProperty("os.name").contains("Windows")))?true:false;
+			String fileSep = isWindows?"\\":"/";
+			
+			String s_key="file";
+			
+			try {
+				Instances data = null;
+				
+				String arff_dir = args[0];
+				String csv_dir = WekaMagic.getParent(arff_dir);
+				
+				String speakerTable = args[2];
+				
+				
+				
+				
+				Instances sound = WekaMagic.getSoundInstancesWithFile(arff_dir, csv_dir + "output.csv"); 	
+				Instances text = WekaMagic.textCSVToInstances(csv_dir + "output.csv",s_key);
+				
+				//data = WekaMagic.mergeInstancesBy(sound, text, s_key);
+				
+				System.out.println("whole data size: " + text.size());
+				
+				SpeakerSet speakerData = WekaMagic.matchSpeakerToInstances(speakerTable, text, s_key);
+				
+				speakerData.printInfo();
+				System.out.println();
+				
+				//speakerData.reduceFemale(6, 1);
+				//speakerData.reduceMale(14,2);
+				//speakerData.reduceBySize(1);
+				speakerData.filterByClassBalance(0.5);
+				
+				if(speakerData.getNumberMale()>speakerData.getNumberFemale()){
+					speakerData.reduceMale(speakerData.getNumberMale()-speakerData.getNumberFemale(),2);
+				}
+				else{
+					speakerData.reduceFemale(speakerData.getNumberFemale() - speakerData.getNumberMale(), 2);
+				}
+				
+				speakerData.printInfo();
+				System.out.println();
+				
+				//SpeakerCrossValidation.check();
+				
+				//Filter f = SpeakerCrossValidation.createModel(speakerData, sound, 0.04, s_key);
+				
+				//SpeakerCrossValidation.check();
+				
+				SpeakerCrossValidation.run(speakerData, speakerTable, sound,  s_key);
+				
+				
+				/*
+				Instances [] sets = WekaMagic.getInterspeech2011Sets(args[1], text, s_key, null);
+				
+				Instances train = sets[0];
+				Instances dev   = sets[1];
+				Instances test  = sets[2];
+				
+				
+				
+				//difference between all instances and the instances selected by interspeech 2011
+				sets = WekaMagic.getInterspeech2011SetsWithFile(args[1], text, s_key);
+
+				for(int i=0;i<3;i++){
+					SpeakerSet speakerData = WekaMagic.matchSpeakerToInstances(speakerTable, sets[i], s_key);
+				
+					System.out.println("Details on " + name[i] + " set in the original version");
+					speakerData.printInfo();
+					System.out.println();
+				}
+				System.out.println();
+				
+				
+				
+				Instances notInSets;// = WekaMagic.getOutOfSets(sets, text, s_key);
+				//SpeakerSet speakerDataWott = WekaMagic.matchSpeakerToInstances(speakerTable, notInSets, s_key);
+				//speakerDataWott.printALCDistributionByRecType();
+				
+				
+				
+				//System.out.println("out of interspeech sets - size: " + notInSets.size());
+				//System.out.println(notInSets);
+				
+				
+				
+				//difference between all instances and the set without tongue twisters
+				Instances text_wott = WekaMagic.textCSVToInstances(csv_dir + "output_wott.csv",s_key);
+				
+				notInSets = WekaMagic.getOutOfSets(new Instances [] { text_wott }, text, s_key);				
+				System.out.println("deleted tongue twister - size: " + notInSets.size());
+				//System.out.println(notInSets); //print all affected tongue twisters
+				SpeakerSet speakerDataWott = WekaMagic.matchSpeakerToInstances(speakerTable, notInSets, s_key);
+				speakerDataWott.printALCDistributionByRecType();
+				
+				//delete all tongue twisters in the Interspeech 2011 set
+				
+				Instances [] is11wott = WekaMagic.deleteFromSets(sets, notInSets, s_key);
+				
+				System.out.println();
+				for(int i=0;i<3;i++){
+					SpeakerSet speakerData = WekaMagic.matchSpeakerToInstances(speakerTable, is11wott[i], s_key);
+				
+					System.out.println("Details on " + name[i] + "set without tongue twisters:");
+					speakerData.printInfo();
+					System.out.println();
+				}
+				System.out.println();
+
+			
+				
+				//category				
+				String category_file = args[3];
+				
+				Instances [] cat = WekaMagic.getInterspeech11ByCategory(is11wott,s_key,category_file);
+				
+				Instances [] catwott = WekaMagic.deleteFromSets(cat, notInSets, s_key);
+				
+				System.out.println();
+				for(int i=0;i<3;i++){
+					SpeakerSet speakerData = WekaMagic.matchSpeakerToInstances(speakerTable, catwott[i], s_key);
+				
+					System.out.println("Details on " + name[i] + "set without tongue twisters:");
+					speakerData.printInfo();
+					System.out.println();
+				}
+				System.out.println();
+				
+				
+				
+				*/
+				
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
+}
--- src/team2014/weka/speaker/SpeakerSet.java
+++ src/team2014/weka/speaker/SpeakerSet.java
@@ -3,6 +3,8 @@ package team2014.weka.speaker;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import weka.core.Instances;
+
 public class SpeakerSet {
 	private ArrayList<SpeakerSamples> speakerSamples;
 
@@ -103,6 +105,172 @@ public class SpeakerSet {
 		return list;
 	}
 	
+	public void reduceMale(int male,int size){
+		int count=0;
+		for(int i=0;i<speakerSamples.size();i++){
+			SpeakerSamples sp = speakerSamples.get(i);
+			if(sp.getSpeaker().isMale() && sp.getSize() == size){
+				//System.out.println("m: " + sp.getSize());
+				speakerSamples.remove(i);
+				i--;
+				count++;
+				if(count>= male) break;
+				
+			}
+		}
+	}
+	
+	public void reduceFemale(int female,int size){
+		int count=0;
+		for(int i=0;i<speakerSamples.size();i++){
+			SpeakerSamples sp = speakerSamples.get(i);
+			if(sp.getSpeaker().isFemale() && sp.getSize() == size){
+				//System.out.println("m: " + sp.getSize());
+				speakerSamples.remove(i);
+				i--;
+				count++;
+				if(count>= female) break;
+				
+			}
+		}
+	}
+	
+	public Instances buildTest(Instances data,String key, int numberSpeaker){
+		Instances test = new Instances(data,-1);
+		ArrayList<String> subset = new ArrayList<String>();
+		
+		int female=-1; Boolean fbool = false;
+		int male =-1; Boolean mbool = false;
+		
+		for(int i=0;i<speakerSamples.size();i++){
+			if(speakerSamples.get(i).getSpeaker().isFemale()){
+				female++;
+			}
+			else{
+				male++;
+			}
+			
+			if((male >= 0) && (male < numberSpeaker) && speakerSamples.get(i).getSpeaker().isMale()){	
+				for(Sample s:speakerSamples.get(i).getFiles()){
+					subset.add(s.getFilename());
+				}
+				speakerSamples.remove(i);
+				i--;
+				//System.out.println("i-test: " +i);
+			}	
+			if((female >= 0) && (female < numberSpeaker) && speakerSamples.get(i).getSpeaker().isFemale()){
+				for(Sample s:speakerSamples.get(i).getFiles()){
+					subset.add(s.getFilename());
+				}	
+				speakerSamples.remove(i);
+				i--;
+				//System.out.println("i-test: " +i);
+			}
+			
+		}
+		
+		for(int i=0;i<data.size();i++){
+			for(String in_key:subset){
+				if(data.get(i).stringValue(data.attribute(key)).equals(in_key)){
+					test.add(data.get(i));
+					break;
+				}
+			}
+		}
+		return test;
+	}
+	
+	
+	
+	public Instances getTest(int fold, Instances data,String key, int numberSpeaker){
+		Instances test = new Instances(data,-1);
+		ArrayList<String> subset = new ArrayList<String>();
+		
+		int female=-1; Boolean fbool = false;
+		int male =-1; Boolean mbool = false;
+		
+		for(int i=0;i<speakerSamples.size();i++){
+			if(speakerSamples.get(i).getSpeaker().isFemale()){
+				female++;
+			}
+			else{
+				male++;
+			}
+			
+			if((male >= fold*numberSpeaker) && (male < (fold+1)*numberSpeaker) && speakerSamples.get(i).getSpeaker().isMale()){	
+				for(Sample s:speakerSamples.get(i).getFiles()){
+					subset.add(s.getFilename());
+				}
+				//System.out.println("i-test: " +i);
+			}	
+			if((female >= fold*numberSpeaker) && (female < (fold+1)*numberSpeaker) && speakerSamples.get(i).getSpeaker().isFemale()){
+				for(Sample s:speakerSamples.get(i).getFiles()){
+					subset.add(s.getFilename());
+				}			
+				//System.out.println("i-test: " +i);
+			}
+		}
+		
+		for(int i=0;i<data.size();i++){
+			for(String in_key:subset){
+				if(data.get(i).stringValue(data.attribute(key)).equals(in_key)){
+					test.add(data.get(i));
+					break;
+				}
+			}
+		}
+		return test;
+	}
+	public Instances getTrain(int fold, Instances data,String key, int numberSpeaker){
+		Instances train = new Instances(data,-1);
+		ArrayList<String> subset = new ArrayList<String>();
+		
+		int female=-1; 
+		int male =-1; 
+		
+		for(int i=0;i<speakerSamples.size();i++){
+			if(speakerSamples.get(i).getSpeaker().isFemale()){
+				female++;
+			}
+			else{
+				male++;
+			}
+			if((male >= fold*numberSpeaker) && (male < (fold+1)*numberSpeaker) && speakerSamples.get(i).getSpeaker().isMale()){	
+				//System.out.println("i-train: " +i);
+			}	
+			else if((female >= fold*numberSpeaker) && (female < (fold+1)*numberSpeaker) && speakerSamples.get(i).getSpeaker().isFemale()){
+				//System.out.println("i-train: " +i);
+			}
+			else{
+				for(Sample s:speakerSamples.get(i).getFiles()){
+					subset.add(s.getFilename());
+				}
+			}
+		}
+		
+		for(int i=0;i<data.size();i++){
+			for(String in_key:subset){
+				if(data.get(i).stringValue(data.attribute(key)).equals(in_key)){
+					train.add(data.get(i));
+					break;
+				}
+			}
+		}
+		return train;
+	}
+	
+	public void reduceBySize(int size){
+		for(int i=0;i<speakerSamples.size();i++){
+			SpeakerSamples sp = speakerSamples.get(i);
+			if(sp.getSize() == size){
+				//System.out.println("m: " + sp.getSize());
+				speakerSamples.remove(i);
+				i--;
+				
+			}
+		}
+	}
+	
 	public void printALCDistributionByRecType(){
 		HashSet<Integer> list = getDistincRecTypeSet();
 		
@@ -122,6 +290,18 @@ public class SpeakerSet {
 		}
 		
 		System.out.println("\ntotal: " + total + "\n\n");
+	}
+
+	public void filterByClassBalance(double d) {
+		for(int i=0;i<speakerSamples.size();i++){
+			SpeakerSamples sp = speakerSamples.get(i);
+			if((double)sp.getNumberOfIntoxicated()/sp.getSize() != d){
+				//System.out.println("m: " + sp.getSize());
+				speakerSamples.remove(i);
+				i--;
+				
+			}
+		}
 	}
 	
 	
